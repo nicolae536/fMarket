200b084878db9c8eb8b2676c6c737c4f0488eb51
Luci Baciu <lucybaciu2006@gmail.com>
2016-05-05 00:27:49 +0300

    security

--- fmarket/src/main/java/ro/fmarket/security/SecurityConfig.java
+++ fmarket/src/main/java/ro/fmarket/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package ro.fmarket.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,6 +17,7 @@ import ro.fmarket.model.account.AccountService;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -41,7 +43,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/").permitAll()
+				.antMatchers("/app/pages/adminPage/**", "/admin/**").hasRole("ADMIN")
+				.antMatchers("/app/pages/accountSettingsPage/**").hasRole("USER")
+				.antMatchers("/app/**", "/", "/login").permitAll()
 				.anyRequest().authenticated().and()
 		         .formLogin()
 		         	.successHandler(new RestAuthenticationSuccessHandler(new SavedRequestAwareAuthenticationSuccessHandler()))
