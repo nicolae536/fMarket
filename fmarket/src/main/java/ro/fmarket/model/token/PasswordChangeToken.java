6872af34f5d8c3f215bfcf1004e2ff4773922651
Luci Baciu <lucybaciu2006@gmail.com>
2016-05-06 23:07:26 +0300

    password change token fix

--- fmarket/src/main/java/ro/fmarket/model/token/PasswordChangeToken.java
+++ fmarket/src/main/java/ro/fmarket/model/token/PasswordChangeToken.java
@@ -14,7 +14,7 @@ public class PasswordChangeToken extends TokenEntity {
 	@ManyToOne(optional = false)
 	private Account account;
 	
-	@Column(nullable = false, columnDefinition = "binary", length = 70)
+	@Column(nullable = false, columnDefinition = "binary(60)")
 	private String newPassword;
 	
 }
