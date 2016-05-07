534ba95ce3fd51cfa6888d31fdbe82c514aeb06a
Luci Baciu <lucybaciu2006@gmail.com>
2016-05-07 13:48:47 +0300

    database config change

---
+++ fmarket/src/main/java/ro/fmarket/config/DatabaseConfig.java
@@ -0,0 +1,10 @@
+package ro.fmarket.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+
+@Configuration
+public class DatabaseConfig extends HikariConfig {
+
+}
