93d5b25aaa54ccf0a1cc033011772f0885fce26a
Luci Baciu <lucybaciu2006@gmail.com>
2016-05-12 16:28:35 +0300

    some tests

--- fmarket/src/main/java/ro/fmarket/TestController.java
+++ fmarket/src/main/java/ro/fmarket/TestController.java
@@ -1,21 +1,34 @@
 package ro.fmarket;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import ro.fmarket.mail.MailServiceImpl;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
-//	@Autowired
-//	private MailServiceImpl mailService;
-	
-	@RequestMapping(value = "/mail")
-	public void sendMail() {
-//		mailService.sendPasswordChangeMail("lucybaciu2006@gmail.com", "123456");
+	// @Autowired
+	// private MailServiceImpl mailService;
+
+	@Autowired
+	private SessionRegistry sessionRegistry;
+
+	@RequestMapping(value = "/useri")
+	public List<String> sendMail() {
+		List<String> usersNamesList = new ArrayList<String>();
+		List<Object> principals = sessionRegistry.getAllPrincipals();
+		for (Object principal : principals) {
+			if (principal instanceof User) {
+				usersNamesList.add(((User) principal).getUsername());
+			}
+		}
+		return usersNamesList;
 	}
-	
+
 }
