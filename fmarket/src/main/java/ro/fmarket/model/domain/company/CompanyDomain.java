f247ea6716415a8039d37060bd8fbcc3c82e16f3
Luci Baciu <lucybaciu2006@gmail.com>
2016-05-29 22:44:57 +0300

    small changes

--- fmarket/src/main/java/ro/fmarket/model/domain/company/CompanyDomain.java
+++ fmarket/src/main/java/ro/fmarket/model/domain/company/CompanyDomain.java
@@ -8,11 +8,14 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import ro.fmarket.core.base.BaseEntity;
 import ro.fmarket.model.company.Company;
+import ro.fmarket.model.demand.Demand;
 
 @Data
 @Entity
+@EqualsAndHashCode(callSuper = false, exclude = "companies")
 public class CompanyDomain extends BaseEntity {
 
 	@Column(length = 30, nullable = false)
