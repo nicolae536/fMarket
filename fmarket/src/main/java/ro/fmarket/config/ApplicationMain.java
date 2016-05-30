0b11044d60e944715922a06aa6bb194fda831a4e
Luci Baciu <lucybaciu2006@gmail.com>
2016-05-30 10:46:59 +0300

    datasource autoconfig exclude

--- fmarket/src/main/java/ro/fmarket/config/ApplicationMain.java
+++ fmarket/src/main/java/ro/fmarket/config/ApplicationMain.java
@@ -3,9 +3,10 @@ package ro.fmarket.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class})
 @EnableAutoConfiguration
 public class ApplicationMain {
 
