df937e25a2b623f838ed397d74b2859e1b1115f2
Lucian Baciu <lucian.baciu@servustech.com>
2016-06-18 14:18:07 +0300

    small changes

--- fmarket/src/main/java/ro/fmarket/config/AppConfig.java
+++ fmarket/src/main/java/ro/fmarket/config/AppConfig.java
@@ -6,12 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 
 @Configuration
 @ComponentScan("ro.fmarket")
@@ -20,7 +18,6 @@ import org.springframework.web.multipart.support.StandardServletMultipartResolve
 @EnableConfigurationProperties
 @EnableTransactionManagement
 @EnableAspectJAutoProxy
-@EnableJpaRepositories
 public class AppConfig {
 
 	@Bean
@@ -28,13 +25,4 @@ public class AppConfig {
 		return new BCryptPasswordEncoder();
 	}
 	
-//	@Autowired
-//	private DataSource dataSource;
-//	
-//	@Autowired
-//	private EntityManagerFactory factory;
-	
-//	@PersistenceContext
-//	private EntityManager manager;
-//	
 }
